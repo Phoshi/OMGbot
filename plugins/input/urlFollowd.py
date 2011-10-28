afa859e0cec5afbc194aa1984580e8b4513bcb3b
Phoshi <AHPhoshi@gmail.com>
2011-10-28 11:22:39 +0100

    Helper plugin to desynchronise urlFollowing

---
+++ plugins/input/urlFollowd.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from plugins import plugin
+import time
+import globalv
+import urllib2
+import json
+import Queue
+import traceback
+from pluginFormatter import formatOutput
+
+class asyncInput(object):
+    def __init__(self,outputQueue, inputQueue):
+        running = True
+        self.Queue=outputQueue
+        urlQueue = Queue.Queue()
+        globalv.variables["urlFollowQueue"] = urlQueue
+
+        while running:
+            while not inputQueue.empty():
+                data = inputQueue.get()
+                if data == "stop":
+                    running = False
+            newData = urlQueue.get()
+            complete, outputFunction = newData
+            try:
+                returnData = formatOutput(outputFunction(complete), complete)[0]
+                if returnData!="":
+                    outputQueue.put("#%s\r\n"%returnData)
+            except:
+                print "urlFollowd failued:",complete.complete()
+
+            
+        del globalv.variables["urlFollowQueue"]
+    def gettype(self):
+        return "input"
+    def describe(self, complete):
+        return ["PRIVMSG $C$ :I SAY X EVERY Y"]
